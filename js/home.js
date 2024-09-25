cfc738c9b454c5192f31ef02a405d3495758a68c
Ridwanul-hoque <rhr06junhoque@gmail.com>
2024-09-25 19:11:58 +0600

    sticky navbar

--- js/home.js
+++ js/home.js
@@ -1,3 +1,13 @@
 document.getElementById('btn-blog').addEventListener('click',function(){
     window.location.href = '/blog.html'
 })
+
+window.addEventListener('scroll', function() {
+    const header = document.getElementById('header-blur');
+  
+    if (window.scrollY > 0) {
+      header.classList.add('backdrop-blur-xl', 'bg-white/70');
+    } else {
+      header.classList.remove('backdrop-blur-xl', 'bg-white/70');
+    }
+  });
